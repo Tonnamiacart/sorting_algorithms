b5f0835bd18d514499ed615db3e1d150637d051f
GODSPE1 <godspy18@gmail.com>
2024-01-23 12:44:31 -0800

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, i;
+	if (size < 2)
+	{return;
+	}
 
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
