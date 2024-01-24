a5275c599628644f912c3a1aea41c288dabf3365
GODSPE1 <godspy18@gmail.com>
2024-01-23 20:00:29 -0800

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,35 +3,18 @@
 #include "sort.h"
 
 /**
- * swap - this swaps two integers in an array
- * @ls_vl: the first integer to be swapped
- *
- * @hg_vl: the second integer to be swapped
- */
-void swap(int *ls_vl, int *hg_vl)
-{
-	int temp;
-
-	temp = *ls_vl;
-	*ls_vl = *hg_vl;
-	*hg_vl = temp;
-}
-
-/**
  * selection_sort - Sort an array of integers in ascending order
  * using the selection sort algorithm
  * @array : array of integer
- * @size size of array
+ * @size : size of array
  * Description : This prints the array after each swap is done
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t j, i, min_value
+	size_t j, i, min_value, temp;
 
-		if (size < 2)
-		{
+		if (array == NULL || size < 2)
 			return;
-		}
 
 
 	for (i = 0; i < size; i++)
@@ -40,14 +23,17 @@ void selection_sort(int *array, size_t size)
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
+			if (array[j] < array[min_value])
 			{
 				min_value = j;
 			}
 		}
 		if (min_value != i)
 		{
-			swap(&array[i], &array[min]);
+			temp = array[i];
+			array[i] = array[min_value];
+			array[min_value] = temp;
+
 			print_array(array, size);
 		}
 	}
